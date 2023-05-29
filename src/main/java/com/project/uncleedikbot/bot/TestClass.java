601969ab2829f664ed243efb9523a471874961ae
roman <romanze1706@gmail.com>
2023-05-29 21:24:02 +0300

    test

--- src/main/java/com/project/uncleedikbot/bot/TestClass.java
+++ src/main/java/com/project/uncleedikbot/bot/TestClass.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.File;
 
 @Component
 public class TestClass {
@@ -13,7 +17,17 @@ public class TestClass {
     public SendMessage testMessage(Update update) {
         SendMessage message = new SendMessage();
         message.setChatId(update.getMessage().getChatId());
-        message.setText("TEST");
+        message.setText("Это тестовая команда");
         return message;
+
     }
+
+    public SendSticker sendSticker(Update update) {
+
+        SendSticker sticker = new SendSticker();
+        sticker.setChatId(update.getMessage().getChatId());
+        sticker.setSticker(new InputFile(new File("stickers/sticker.webp")));
+        return sticker;
+    }
+
 }
