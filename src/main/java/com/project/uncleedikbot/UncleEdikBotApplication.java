ee3ed238ba5f4ddeeaa71568b1843f82402532f3
roman <romanze1706@gmail.com>
2023-05-29 17:19:59 +0300

    Созданы базовые настройки проекта

---
+++ src/main/java/com/project/uncleedikbot/UncleEdikBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.uncleedikbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncleEdikBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UncleEdikBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/uncleedikbot/UncleEdikBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uncleedikbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncleEdikBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
